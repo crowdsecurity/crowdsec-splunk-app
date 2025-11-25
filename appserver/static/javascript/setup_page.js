bd17934a039bb878a313860fd934f582ce2c6cc8
Sebastien Blot <sebastien@crowdsec.net>
2025-11-25 10:22:49 +0100

    bump version to 1.2.3

--- appserver/static/javascript/setup_page.js
+++ appserver/static/javascript/setup_page.js
@@ -3,21 +3,16 @@
 var app_name = "./crowdsec-splunk-app";
 
 require.config({
-    paths: {
-        myApp: "../app/" + app_name + "/javascript/views/app",
-        react: "../app/" + app_name + "/javascript/vendor/react.production.min",
-        ReactDOM: "../app/" + app_name + "/javascript/vendor/react-dom.production.min",
-    },
-    scriptType: "module",
-    urlArgs: "v=v1.2.2"
+  paths: {
+    myApp: "../app/" + app_name + "/javascript/views/app",
+    react: "../app/" + app_name + "/javascript/vendor/react.production.min",
+    ReactDOM:
+      "../app/" + app_name + "/javascript/vendor/react-dom.production.min",
+  },
+  scriptType: "module",
+  urlArgs: "v=v1.2.3",
 });
 
-require([
-    "react",
-    "ReactDOM",
-    "myApp",
-], function (react, ReactDOM, myApp) {
-    ReactDOM.render(myApp, document.getElementById('main_container'));
-})
-
-;
+require(["react", "ReactDOM", "myApp"], function (react, ReactDOM, myApp) {
+  ReactDOM.render(myApp, document.getElementById("main_container"));
+});
